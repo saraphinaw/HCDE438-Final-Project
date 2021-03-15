47d36569c2879eee37a562c3eaab4565167e1369
saraphw@uw.edu <saraphinawang@Saraphinas-MacBook-Pro.local>
2021-03-14 17:37:40 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import { AiOutlinePlus } from "react-icons/ai";
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Draggable from "react-draggable";
+import { v4 as uuidv4 } from "uuid";
+var randomColor = require("randomcolor");
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+  const [item, setItem] = useState("");
+  const [items, setItems] = useState(
+    JSON.parse(localStorage.getItem("items")) || []
+  );
+
+  const newitem = () => {
+    if (item.trim() !== "") {
+      const newitem = {
+        id: uuidv4(),
+        item: item,
+        color: randomColor({
+          luminosity: "light",
+        }),
+        defaultPos: { x: 550, y: 300 },
+      };
+      setItems((items) => [...items, newitem]);
+      setItem("");
+    } else {
+      alert("Enter a item");
+      setItem("");
+    }
+  };
+
+  const keyPress = (event) => {
+    var code = event.keyCode || event.which;
+    if (code === 13) {
+      newitem();
+    }
+  };
+
+  useEffect(() => {
+    localStorage.setItem("items", JSON.stringify(items));
+  }, [items]);
+
+  const updatePos = (data, index) => {
+    let newArr = [...items];
+    newArr[index].defaultPos = { x: data.x, y: data.y };
+    setItems(newArr);
+  };
+
+  const deleteNote = (id) => {
+    setItems(items.filter((item) => item.id !== id));
+  };
+
+  return (<div className="MyApp">
+      <header className="header">
+        <div className="logo" />
+        Quotes
       </header>
+      <div className="new-item">
+        <input className="Input"
+          value={item}
+          onChange={(e) => setItem(e.target.value)}
+          placeholder="Enter a quote..."
+          onKeyPress={(e) => keyPress(e)}
+        />
+        <button className="button" onClick={newitem}>
+          <AiOutlinePlus>
+            style={{minWidth:'0.7rem',marginLeft:2}} 
+          </AiOutlinePlus>
+        </button>
+      </div>
+      {items.map((item, index) => {
+        return (
+          <Draggable
+            key={item.id}
+            defaultPosition={item.defaultPos}
+            onStop={(e, data) => {
+              updatePos(data, index);
+            }}
+          >
+            <div style={{ backgroundColor: item.color }} className="box">
+              {`${item.item}`}
+              <button className="delete" onClick={(e) => deleteNote(item.id)}>
+                X
+              </button>
+            </div>
+          </Draggable>
+        );
+      })}
     </div>
   );
 }
